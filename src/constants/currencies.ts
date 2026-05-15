cc66440bf9e1900065b61de7eafe82ed28744506
Anurag <anuragyadav2516@gmail.com>
2026-05-16 00:59:31 +0530

    feat: add form status and currencies

---
+++ src/constants/currencies.ts
@@ -0,0 +1,5 @@
+export const currencyOptions = [
+  { value: "INR", label: "₹ INR" },
+  { value: "USD", label: "$ USD" },
+  { value: "EUR", label: "€ EUR" },
+];
---
+++ src/constants/statusOptions.ts
@@ -0,0 +1,7 @@
+export const statusOptions = [
+    {value: "Draft", label: "Draft"},
+    {value: "Sent", label: "Sent"},
+    {value: "Paid", label: "Paid"},
+    {value: "Overdue", label: "Overdue"},
+]
+
