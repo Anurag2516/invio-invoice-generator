2ff0e47b0151bdcb4e4d0730af9090808dc0e4c0
Anurag <anuragyadav2516@gmail.com>
2026-06-01 00:58:45 +0530

    feat: remove divider

--- src/components/ui/SectionHeader.tsx
+++ src/components/ui/SectionHeader.tsx
@@ -5,18 +5,13 @@ interface SectionHeaderProps {
 
 const SectionHeader = ({ label, subLabel }: SectionHeaderProps) => {
   return (
-    <div className="flex items-center gap-3 w-full">
-      <div className="flex gap-2 items-center">
-        <h2 className="text-sm text-brand uppercase font-bold tracking-wider">
-          {label}
-        </h2>
-        {subLabel && (
-          <p className="text-xs text-stone-500 tracking-wide">
-            {subLabel}
-          </p>
-        )}
-      </div>
-      <div className="flex-1 h-[1.8px] bg-[#d4cbbf]" />
+    <div className="flex gap-2 items-center">
+      <h2 className="text-sm text-stone uppercase font-bold tracking-wider">
+        {label}
+      </h2>
+      {subLabel && (
+        <p className="text-xs text-stone-500 tracking-wide">{subLabel}</p>
+      )}
     </div>
   );
 };
