4e18839b65e5a685d639648f6157c9e5f67640f7
Anurag <anuragyadav2516@gmail.com>
2026-06-07 14:03:18 +0530

    feat: pass error to lineitems fields

--- src/components/invoice-form/LineItemsTable.tsx
+++ src/components/invoice-form/LineItemsTable.tsx
@@ -86,6 +86,7 @@ const LineItemsTable = ({ register, control, errors }: InvoiceFormProps) => {
                       onChange={(e) => {
                         if (positiveNumberFilter(e)) field.onChange(e);
                       }}
+                      error={errors?.lineItems?.[index]?.rate?.message}
                     />
                   )}
                 />
@@ -105,6 +106,7 @@ const LineItemsTable = ({ register, control, errors }: InvoiceFormProps) => {
                       onChange={(e) => {
                         if (positiveNumberFilter(e)) field.onChange(e);
                       }}
+                      error={errors?.lineItems?.[index]?.quantity?.message}
                     />
                   )}
                 />
