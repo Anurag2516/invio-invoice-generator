bc8c60161eebccb0c7f0dceae84b2ffe0787c017
Anurag <anuragyadav2516@gmail.com>
2026-06-14 00:13:14 +0530

    chore: replace old fonts with IBM Plex font family

--- src/components/invoice-pdf/PDFFonts.ts
+++ src/components/invoice-pdf/PDFFonts.ts
@@ -1,12 +1,8 @@
 import { Font } from "@react-pdf/renderer";
-import DMSans from "../../assets/fonts/DMSans_24pt-Regular.ttf";
-import DMSerifDisplay from "../../assets/fonts/DMSerifDisplay-Regular.ttf";
-import DMSerifDisplayItalic from "../../assets/fonts/DMSerifDisplay-Italic.ttf";
-import Epilogue from "../../assets/fonts/Epilogue-Regular.ttf";
+import IBMPlexSansItalic from "../../assets/fonts/IBMPlexSans-Italic.ttf";
+import IBMPlexSans from "../../assets/fonts/IBMPlexSans-Regular.ttf";
 import IBMPlexMono from "../../assets/fonts/IBMPlexMono-Regular.ttf";
 
-Font.register({ family: "DM Sans", src: DMSans });
-Font.register({ family: "DM Serif Display", src: DMSerifDisplay });
-Font.register({ family: "DM Serif Display Italic", src: DMSerifDisplayItalic });
-Font.register({ family: "Epilogue", src: Epilogue });
 Font.register({ family: "IBM Plex Mono", src: IBMPlexMono });
+Font.register({ family: "IBM Plex Sans", src: IBMPlexSans });
+Font.register({ family: "IBM Plex Sans Italic", src: IBMPlexSansItalic });
