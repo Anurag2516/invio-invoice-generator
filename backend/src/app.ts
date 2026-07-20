d48097d70ae210f72ef984d9d676d95c9321f17e
Anurag <anuragyadav2516@gmail.com>
2026-07-21 00:40:13 +0530

    feat: add auth and invoice routes with cookie parser middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import authRoutes from './routes/auth.routes'
+import invoiceRoutes from "./routes/invoice.routes";
+import cookieParser from 'cookie-parser';
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
+
+app.use("/api/auth", authRoutes)
+app.use("/api/invoice", invoiceRoutes);
+
 
 export default app;
